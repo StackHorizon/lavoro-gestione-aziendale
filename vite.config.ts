57a8c0c327432cb5fac18b323aab39901e1692fe
strazzullociroandrea <ciroandreastrazzullo@icloud.com>
2025-05-30 17:30:24 +0200

    prova evito refresh e torno indietro

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // <-- qui imposti la base path per GitHub Pages
+  //base: "/", // <-- qui imposti la base path per GitHub Pages
 
   server: {
     host: "::",
